6960465215c56f270a1d4d5e1fe12cf711f7540e
John Ramsey <19388331+Diggs27@users.noreply.github.com>
2022-12-04 22:01:55 -0500

    AOC Day 2

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,63 @@ use std::path::Path;
 
 
 fn main() {
+    aoc2();
+}
+
+fn aoc2(){
+    if let Ok(lines) = read_lines("./aoc2") {
+        // Consumes the iterator, returns an (Optional) String
+        let mut count: i32 = 0;
+        let mut total: i32 = 0;
+        for line in lines {
+            if let Ok(ip) = line {
+                let split = ip.split(" ");
+                let vec = split.collect::<Vec<&str>>();
+                
+                let r: i32 = get_test(vec[0], vec[1]);
+                println!("{} {} = {} ", vec[0], vec[1], r);
+                
+                total = total + r;
+              
+
+            }
+         count = count + 1;
+        
+        }
+        println!("Total {}", total);
+    }
+}
+
+fn get_test(opp: &str, play: &str) -> (i32) {
+    let mut res: i32 = 0;
+    match opp {
+        "A" => { match play { 
+            "X" => res = 0 + 3, 
+            "Y" => res = 3 + 1,
+            "Z" => res = 6 + 2,
+            _ => println!("Ain't special"),
+            };
+        }
+        "B" => { match play { 
+            "X" => res = 0 + 1,
+            "Y" =>  res = 3  + 2,
+            "Z" => res =6  + 3,
+            _ => println!("Ain't special"),
+            };
+        }
+        "C" => { match play { 
+            "X" =>  res = 0 + 2,
+            "Y" =>  res = 3 + 3,
+            "Z" =>  res = 6 + 1, 
+            _ => println!("Ain't special"),
+            };
+        }
+        _ => println!("Ain't special"),
+    }
+    return res;
+}
+
+fn aoc1(){
     let mut vec: Vec<i32> = Vec::new();
     let mut counter: i32 = 0;
     let mut total: i32 = 0;
